4861c3752a8811b4b09b6f52387939da5c7450c4
venkatesh-yarra07 <venkyy2107@gmail.com>
2025-06-20 12:27:12 +0530

    fix error app

--- src/main/java/com/example/demo_app/App.java
+++ src/main/java/com/example/demo_app/App.java
@@ -1,7 +1,7 @@
-package com.example;
+package com.example.demo_app;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Maven project!");
-    }
+    }
 }
