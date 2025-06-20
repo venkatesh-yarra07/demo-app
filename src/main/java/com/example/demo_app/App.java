ba6a0c7a3e345da83c746ae78c9d30007a3fe377
venkatesh-yarra07 <venkyy2107@gmail.com>
2025-06-20 14:29:50 +0530

    app.java updated

--- src/main/java/com/example/demo_app/App.java
+++ src/main/java/com/example/demo_app/App.java
@@ -1,7 +1,17 @@
 package com.example.demo_app;
 
 public class App {
-	public static void main(String[] args) {
-		System.out.println("Hello from Maven Project");
-	}
+    public static void main(String[] args) {
+        System.out.println("✅ Application started successfully!");
+        // Add your app logic here
+        while (true) {
+            try {
+                Thread.sleep(10000);
+                System.out.println("⏳ App is still running...");
+            } catch (InterruptedException e) {
+                System.err.println("⚠ Interrupted!");
+                break;
+            }
+        }
+    }
 }
